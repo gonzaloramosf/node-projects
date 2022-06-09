78a013b6d87084852400bee87295f748e90e6564
gonzaloramosf <gonza.farinho@hotmail.com>
2022-06-09 17:58:59 -0300

    serverWithExpress

---
+++ serverWithExpress/serverWithExpress.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const fs = require('fs');
+const productsContainer = [{"title":"auto","id":1},{"title":"camioneta","id":2},{"title":"moto","id":3}];
+
+class Container {
+    async getAll() {
+        try {
+            let productsFile = await fs.promises.writeFile('./products.txt', JSON.stringify(productsContainer));
+            productsFile = await fs.promises.readFile('./products.txt', 'utf-8');
+            productsFile = JSON.parse(productsFile);
+            console.log("getAll: ", productsFile);
+            return productsFile;   
+        } catch (error) {
+            console.log(`Error getAll method: ${error}`);
+        }
+    }
+
+    async getRandomProduct() {
+        try {
+            let productsFile = await fs.promises.writeFile('./products.txt', JSON.stringify(productsContainer));
+            productsFile = await fs.promises.readFile('./products.txt', 'utf-8');
+            productsFile = JSON.parse(productsFile);
+            let productRandom = productsFile[Math.floor(Math.random()*productsFile.length)];
+            console.log(productRandom);
+            return productRandom;
+        } catch (error) {
+            console.log(`Error getRandomProduct method: ${error}`);
+        }
+    }
+}
+
+let box = new Container();
+
+app.get('/products', (req, res) => {
+    let getAll = box.getAll();
+    res.json(getAll);
+})
+app.get('/productRandom', (req, res) => {
+    let productRandom = box.getRandomProduct();
+    res.json(productRandom);
+})
+
+app.listen(port, (error) => {
+    if(!error) {
+        console.log(`Server on port: ${port}`);
+    } else {
+        console.log(`Error: ${error}`)
+    }
+})
